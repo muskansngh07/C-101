50ac72ff29d9a0a9ffec913204294cf63fefc675
Muskan <muskansngh15@gmail.com>
2026-01-31 16:40:25 +0530

    check whether a number is armstrong or not

--- Q1-10/armstrong.c
+++ Q1-10/armstrong.c
@@ -1,19 +1,24 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     int n;
     printf("Enter a number: ");
     scanf("%d",&n);
     int temp=n;
-    int rev=0;
+    int digit=0,sum=0;
     while(n!=0){
         int x=n%10;
-        rev=rev*10+x;
+        digit++;
         n/=10;
     }
-    if(temp==rev){
-        printf("%d is a palindrome number.",temp);
+    int temp1=temp;
+    while(temp!=0){
+        int y=temp%10;
+        sum+=(round(pow(y,digit))); //use round for better precision
+        temp/=10;
     }
-    else{
-       printf("%d is not a palindrome number.",temp); 
-    }
+    if(sum==temp1)
+        printf("%d is an armstrong number.",temp1);
+    else    
+        printf("%d is not an armstrong number.",temp1);
 }
