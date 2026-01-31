a1ce9349173bbe6fee76551802d252d40b6e3537
Muskan <muskansngh15@gmail.com>
2026-01-31 15:31:28 +0530

    find the factorial of a number using for loop

---
+++ Q1-10/factorial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    int fact=1;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+    printf("The factorial of %d is %d\n",n,fact);
+}
