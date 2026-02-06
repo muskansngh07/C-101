51f2ba7bca2fd5f4af6b00ba750d9551403086a9
Muskan <muskansngh15@gmail.com>
2026-02-07 00:02:41 +0530

    check whether matrix is identity or not

--- Q21-30/identity_matrix.c
+++ Q21-30/identity_matrix.c
@@ -11,7 +11,6 @@ int main(){
         return 0;
     }
     int a[r1][c1];
-    int sum=0;
     printf("Enter the elements of the matrix: \n");
     for(int i=0;i<r1;i++){
         for(int j=0;j<c1;j++){
@@ -35,5 +34,5 @@ int main(){
 
         }
     }
-    printf("The sum of principal diagonal elements is %d\n",sum);
+    printf("The matrix is an identity matrix");
 }
